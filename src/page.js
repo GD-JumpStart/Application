0d8a870d674b80a21bd6f5b4cb49f1c85ccb8e33
ninXout <58670749+ninXout@users.noreply.github.com>
2022-12-23 22:03:44 -0500

    Disable/Delete Menu Fixed and Other Misc Fixes

--- src/page.js
+++ src/page.js
@@ -186,6 +186,127 @@ const page = async (pg) => {
                     window.close();
                 })
                 break
+            case 'texturePacks':
+                document.querySelector('body > main').innerHTML = `<div style="display: flex; padding: 14px; flex-wrap: wrap; align-content: flex-start">
+            <div id="library">
+                <div>
+                    <span></span>
+                    <span>Pack Name</span>
+                    <span>Version</span>
+                    <span>Last Modified</span>
+                </div>
+            </div>
+        </div>`
+
+        let _mods2 = Object.keys(packs)
+        for (let i = 0; i < _mods2.length; i++) {
+            let mod2 = _mods2[i]
+            let version2 = `${Math.floor(Math.random() * 5)}.${Math.floor(Math.random() * 12)}.${Math.floor(Math.random() * 30)}`
+            document.getElementById('library').innerHTML += `<div data-modid="${i}">
+                <span><img src="../assets/defaultmod.png" alt="${mod2}'s icon" style="border-radius: 11px; ${packs[mod2].enabled ? '' : 'filter: grayscale(1)'}" height="60" width="60"></span>
+                <span title="${mod2}">${mod2}</span>
+                <span title="v${version2}">v${version2}</span>
+                <span title="${new Date(mods[mod2].time).toLocaleString()} (${getRelativeTime(packs[mod2].time)})">${getRelativeTime(packs[mod2].time)}</span>
+            </div>`
+        }
+        for (let i = 0; i < _mods2.length; i++) {
+            document.querySelector(`#library div[data-modid="${i}"]`).addEventListener('contextmenu', (e) => {
+                let context = document.getElementsByTagName('context')[0]
+                context.innerHTML = ''
+                // let update = context.appendChild(document.createElement('button'))
+                let disable = context.appendChild(document.createElement('button'))
+                let del = context.appendChild(document.createElement('button'))
+                // update.innerText = 'Update'
+                if (packs[_mods2[i]].enabled) disable.innerText = 'Disable'
+                else disable.innerText = 'Enable'
+                del.innerText = 'Delete'
+                del.id = 'del'
+                context.style.top = e.clientY + 10 + 'px'
+                context.style.left = e.clientX + 10 + 'px'
+                let bounding = context.getBoundingClientRect()
+                if (bounding.bottom >= (window.innerHeight - 5 || document.documentElement.clientHeight - 5)) {
+                    context.style.top = e.clientY - bounding.height - 5 + 'px'
+                }
+                if (bounding.right >= (window.innerWidth - 5 || document.documentElement.clientWidth - 5)) {
+                    context.style.left = e.clientX - bounding.width - 5 + 'px'
+                }
+                context.style.display = 'flex'
+
+                disable.addEventListener('click', () => {
+                    context.style.display = 'none'
+                    if (packs[_mods2[i]].enabled == false) {
+                        packs[_mods2[i]].enabled = true
+                        document.querySelector(`#library div[data-modid="${i}"] span img`).style.filter = ''
+                    } else {
+                        packs[_mods2[i]].enabled = false
+                        document.querySelector(`#library div[data-modid="${i}"] span img`).style.filter = 'grayscale(1)'
+                    }
+
+                    packs[_mods2[i]].enabled
+                    
+                    let list = ''
+                    _mods2.forEach(m => { if (packs[m].enabled) list += m + '.dll\r\n' })
+                    fs.writeFileSync(path.join(localStorage.GDDIR, '/mods.txt'), list)
+                })
+
+                del.addEventListener('click', async () => {
+                    context.style.display = 'none'
+                    const confirm = await modal({ title: 'Confirmation' })
+                    confirm.innerHTML = `<div style="
+                        width: 100%;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        flex-direction: column;
+                    "></div><div style="
+                        padding: 10px;
+                        height: 43px;
+                        border-top: 1px #343a40 solid;
+                        margin-top: 10px;
+                        display: flex;
+                        justify-content: space-between;
+                        align-items: center
+                    ">
+                        <button id="deletemod" class="style red">Delete Mod</button><button id="continue" class="style">Nevermind</button>
+                    </div>`
+
+                    confirm.querySelector('div').innerHTML = `
+                        <p>Are you sure you want to delete <strong>${_mods[i]}</strong>?</p>
+                        <p>This action <strong>cannot</strong> be undone!</p>
+                    `
+
+                    confirm.querySelectorAll('button')[0].addEventListener('click', async () => {
+                        await fs.unlinkSync(path.join(localStorage.GDDIR, `/${_mods2[i]}.geode`))
+                        //let list = ''
+                        //await _mods2.forEach(m => { if (mods[m].enabled && m != _mods2[i]) list += m + '.dll\r\n' })
+                        //await fs.writeFileSync(path.join(localStorage.GDDIR, '/quickldr/settings.txt'), list)
+                        document.querySelector('#modal').style.opacity = '0'
+                        document.querySelector('#modalcontainer').style.opacity = '0'
+                        await wait(200)
+                        document.querySelector('#modalcontainer').style.display = 'none'
+                        document.querySelector(`#library div[data-modid="${i}"]`).style.transition = '200ms ease-out'
+                        document.querySelector(`#library div[data-modid="${i}"]`).style.opacity = '0'
+                        await wait(200)
+                        document.querySelector(`#library div[data-modid="${i}"]`).style.display = 'none'
+                    })
+
+                    confirm.querySelectorAll('button')[1].addEventListener('click', async () => {
+                        document.querySelector('#modal').style.opacity = '0'
+                        document.querySelector('#modalcontainer').style.opacity = '0'
+                        await wait(200)
+                        document.querySelector('#modalcontainer').style.display = 'none'
+                    })
+                })
+            })
+        }
+
+        document.addEventListener('click', e => {
+            if (document.getElementsByTagName('context')[0].contains(e.target)) return;
+            document.getElementsByTagName('context')[0].style.display = 'none'
+        })
+        
+        resolve()
+        break
             default:
 
                 document.querySelector('body > main').innerHTML = '<center></center>'
